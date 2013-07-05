f8bb0e13723fc4d0ec3fdec3424cc3001fa660b5
toulon <coderng@gmail.com>
2013-07-04 20:01:17 -0700

    Changes as of 07/04/2013

---
+++ blog/controllers/homework1.js
@@ -0,0 +1,74 @@
+
+/*
+ * GET homework result 1-2.
+ */
+
+var mongoose = require('mongoose')
+    , FunnyNumber = mongoose.model('FunnyNumber')
+    , Tapes = mongoose.model('Tapes');
+
+exports.tapes = function(req, res){
+    FunnyNumber.find().exec(function(err, numbers){
+        var magic = 0;
+        for (var i = 0 ; i < numbers.length; i++) {
+            if (numbers[i].value % 3 == 0) {
+                magic += numbers[i].value;
+            }
+        }
+
+        res.render('homework1/tapes', { result: magic });
+    });
+};
+
+exports.homework1_3 = function(req, res){
+    var n = req.route.params.n;
+
+    FunnyNumber
+        .find()
+        .limit(1)
+        .skip(n)
+        .sort('value')
+        .exec(function(err, numbers){
+
+            var result;
+            for (var i = 0 ; i < numbers.length; i++) {
+                result = numbers[i].value + '\n';
+            }
+
+            console.log(result);
+            res.render('homework1/hw1-3', { n: n, result: result });
+        });
+};
+
+exports.homework1_3a = function(req, res){
+    var n = req.route.params.n;
+
+    FunnyNumber
+        .find()
+        .limit(1)
+        .skip(n)
+        .sort('value')
+        .exec(function(err, numbers){
+
+            var result;
+            for (var i = 0 ; i < numbers.length; i++) {
+                result = numbers[i].value + '\n';
+            }
+
+            console.log(result);
+            res.render('homework1/hw1-3a', { n: n, result: result });
+        });
+};
+
+exports.homework1_2 = function(req, res){
+    FunnyNumber.find().exec(function(err, numbers){
+        var magic = 0;
+        for (var i = 0 ; i < numbers.length; i++) {
+            if (numbers[i].value % 3 == 0) {
+                magic += numbers[i].value;
+            }
+        }
+
+        res.render('homework1/tapes', { result: magic });
+    });
+};
---
+++ blog/models/tapes.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: toulon
+ * Date: 7/1/13
+ * Time: 9:56 PM
+ * To change this template use File | Settings | File Templates.
+ */
